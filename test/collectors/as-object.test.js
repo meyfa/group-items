1090ef55cceabd2f954995ecb9ccdfec9936a5dc
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-01-05 18:54:51 +0100

    Implement asMap(), asObject(), asTuples()

---
+++ test/collectors/as-object.test.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const { expect } = require("chai");
+
+const asObject = require("../../lib/collectors/as-object.js");
+
+describe("collectors/as-object.js", function () {
+
+    it("returns empty object for empty input", function () {
+        expect(asObject([])).to.deep.equal({});
+    });
+
+    it("returns mapping between key and array of items", function () {
+        expect(asObject([
+            { key: 1, items: [1, 2, 3] },
+            { key: 2, items: [4, 5, 6] },
+        ])).to.deep.equal({
+            1: [1, 2, 3],
+            2: [4, 5, 6],
+        });
+    });
+
+});
---
+++ test/collectors/as-tuples.test.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const { expect } = require("chai");
+
+const asTuples = require("../../lib/collectors/as-tuples.js");
+
+describe("collectors/as-tuples.js", function () {
+
+    it("returns empty array for empty input", function () {
+        expect(asTuples([])).to.deep.equal([]);
+    });
+
+    it("returns array of [key, items] tuples", function () {
+        expect(asTuples([
+            { key: 1, items: [1, 2, 3] },
+            { key: 2, items: [4, 5, 6] },
+        ])).to.deep.equal([
+            [1, [1, 2, 3]],
+            [2, [4, 5, 6]],
+        ]);
+    });
+
+});
