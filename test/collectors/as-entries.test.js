bd814196db408023c357e19fb1e09abe155fdd8e
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-01-05 18:50:01 +0100

    Implement options for asEntries() collector

--- test/collectors/as-entries.test.js
+++ test/collectors/as-entries.test.js
@@ -20,4 +20,34 @@ describe("collectors/as-entries.js", function () {
         ]);
     });
 
+    it("ignores empty options object", function () {
+        expect(asEntries([
+            { key: 1, items: [1, 2, 3] },
+            { key: 2, items: [4, 5, 6] },
+        ], {})).to.deep.equal([
+            { key: 1, items: [1, 2, 3] },
+            { key: 2, items: [4, 5, 6] },
+        ]);
+    });
+
+    it("allows specifying keyName option", function () {
+        expect(asEntries([
+            { key: 1, items: [1, 2, 3] },
+            { key: 2, items: [4, 5, 6] },
+        ], { keyName: "foo" })).to.deep.equal([
+            { foo: 1, items: [1, 2, 3] },
+            { foo: 2, items: [4, 5, 6] },
+        ]);
+    });
+
+    it("allows specifying itemsName option", function () {
+        expect(asEntries([
+            { key: 1, items: [1, 2, 3] },
+            { key: 2, items: [4, 5, 6] },
+        ], { itemsName: "bar" })).to.deep.equal([
+            { key: 1, bar: [1, 2, 3] },
+            { key: 2, bar: [4, 5, 6] },
+        ]);
+    });
+
 });
