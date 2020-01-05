c8ad42c7aacd8a2a8f02bdbd390bc54a2b0a244b
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-01-05 18:58:41 +0100

    Implement asArrays(), keys()

---
+++ test/collectors/as-arrays.test.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const { expect } = require("chai");
+
+const asArrays = require("../../lib/collectors/as-arrays.js");
+
+describe("collectors/as-arrays.js", function () {
+
+    it("returns empty array for empty input", function () {
+        expect(asArrays([])).to.deep.equal([]);
+    });
+
+    it("returns array of item arrays", function () {
+        expect(asArrays([
+            { key: 1, items: [1, 2, 3] },
+            { key: 2, items: [4, 5, 6] },
+        ])).to.deep.equal([
+            [1, 2, 3],
+            [4, 5, 6],
+        ]);
+    });
+
+});
---
+++ test/collectors/keys.test.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const { expect } = require("chai");
+
+const keys = require("../../lib/collectors/keys.js");
+
+describe("collectors/keys.js", function () {
+
+    it("returns empty array for empty input", function () {
+        expect(keys([])).to.deep.equal([]);
+    });
+
+    it("returns array of keys", function () {
+        expect(keys([
+            { key: 1, items: [1, 2, 3] },
+            { key: 2, items: [4, 5, 6] },
+        ])).to.deep.equal([1, 2]);
+    });
+
+});
