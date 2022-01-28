ca8f492a1177022704017f181c3445a2b7ff761e
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-01-28 14:56:57 +0100

    fix: Improve types of asObject collector

--- lib/collectors/as-object.ts
+++ lib/collectors/as-object.ts
@@ -1,13 +1,24 @@
 import { Grouping } from '../types'
 
-export type ObjectCollection<K extends string | number | symbol, V> = Record<K, V[]>
+/**
+ * Determine whether the given key can be used to specify an object property. This is true if, and only if,
+ * it is of type string, number or symbol.
+ *
+ * @param key The key.
+ * @returns Whether the key is of a valid type.
+ */
+function isObjectAssignable<K> (key: K): key is Extract<K, keyof any> {
+  return typeof key === 'string' || typeof key === 'number' || typeof key === 'symbol'
+}
+
+export type ObjectCollection<K extends keyof any, V> = Record<K, V[]>
 
 /**
  * Convert the grouping to an object mapping.
  *
  * @returns The resulting object.
  */
-export type ObjectCollector<K, V> = K extends string | number | symbol ? () => ObjectCollection<K, V> : () => ObjectCollection<string | number | symbol, V>
+export type ObjectCollector<K, V> = () => ObjectCollection<Extract<K, keyof any>, V>
 
 /**
  * Create an ObjectCollector for the given grouping.
@@ -17,9 +28,10 @@ export type ObjectCollector<K, V> = K extends string | number | symbol ? () => O
  */
 export function asObjectFactory<K, V> (groups: Grouping<K, V>): ObjectCollector<K, V> {
   return () => {
-    const obj: ObjectCollection<string | number | symbol, V> = {}
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const obj = {} as ObjectCollection<Extract<K, keyof any>, V>
     for (const group of groups) {
-      if (typeof group.key === 'string' || typeof group.key === 'number' || typeof group.key === 'symbol') {
+      if (isObjectAssignable(group.key)) {
         obj[group.key] = group.items
       }
     }
