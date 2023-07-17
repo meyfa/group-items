754a067625b09ec398876ff89458179bf4008ae6
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2023-07-17 16:34:45 +0200

    fix: Export types with "export type" for clarity (#153)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // export group function and required types
-export { group, Groupable, Collectable, KeyingFunction } from './group.js'
+export { group, type Groupable, type Collectable, type KeyingFunction } from './group.js'
 
 // export collector results
-export { ArraysCollection } from './collectors/as-arrays.js'
-export { EntriesCollectorOptions, EntriesCollection } from './collectors/as-entries.js'
-export { MapCollection } from './collectors/as-map.js'
-export { ObjectCollection } from './collectors/as-object.js'
-export { TuplesCollection } from './collectors/as-tuples.js'
-export { KeysCollection } from './collectors/keys.js'
+export type { ArraysCollection } from './collectors/as-arrays.js'
+export type { EntriesCollectorOptions, EntriesCollection } from './collectors/as-entries.js'
+export type { MapCollection } from './collectors/as-map.js'
+export type { ObjectCollection } from './collectors/as-object.js'
+export type { TuplesCollection } from './collectors/as-tuples.js'
+export type { KeysCollection } from './collectors/keys.js'
